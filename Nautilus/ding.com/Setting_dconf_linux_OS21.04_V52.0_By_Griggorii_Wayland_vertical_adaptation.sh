85d90d478ec68b85c8c4b25ebb1609902a9ca379
Griggorii <40373174+Griggorii@users.noreply.github.com>
2022-01-13 11:41:39 +0800

    Add files via upload

--- Nautilus/ding.com/Setting_dconf_linux_OS21.04_V52.0_By_Griggorii_Wayland_vertical_adaptation.sh
+++ Nautilus/ding.com/Setting_dconf_linux_OS21.04_V52.0_By_Griggorii_Wayland_vertical_adaptation.sh
@@ -11961,6 +11961,10 @@ sudo sh -c "/usr/bin/echo  200000 > /proc/sys/dev/raid/speed_limit_min"
 EOF
 sudo sh -c "/usr/bin/echo 2000000 > /proc/sys/dev/raid/speed_limit_max"
 EOF
+sudo rm /etc/sysctl.d/99-i915psp.conf
+EOF
+sudo bash -c "echo dev.i915.perf_stream_paranoid=0 >> /etc/sysctl.d/99-i915psp.conf"
+EOF
 sudo rm /etc/sysctl.d/raid_speed_limit_min.conf
 EOF
 sudo bash -c "echo dev.raid.speed_limit_min=200000 >> /etc/sysctl.d/raid_speed_limit_min.conf"
@@ -11978,6 +11982,111 @@ EOF
 sudo bash -c "echo >> /etc/sysctl.d/gt_mhz.conf" && sudo echo -e "\ndev.char.226:0.gt_RP1_freq_mhz = 1100\n"|sudo tee --append /etc/sysctl.d/gt_mhz.conf && sudo echo -e "\ndev.char.226:0.gt_RPn_freq_mhz = 1100\n"|sudo tee --append /etc/sysctl.d/gt_mhz.conf && sudo echo -e "\ndev.char.226:0.gt_act_freq_mhz = 1100\n"|sudo tee --append /etc/sysctl.d/gt_mhz.conf && sudo echo -e "\ndev.char.226:0.gt_cur_freq_mhz = 1100\n"|sudo tee --append /etc/sysctl.d/gt_mhz.conf && sudo echo -e "\ndev.char.226:0.gt_min_freq_mhz = 1100\n"|sudo tee --append /etc/sysctl.d/gt_mhz.conf && sudo echo -e "\ndev.char.226:0.gt_RP0_freq_mhz = 2200\n"|sudo tee --append /etc/sysctl.d/gt_mhz.conf && sudo echo -e "\ndev.char.226:0.gt_boost_freq_mhz = 2200\n"|sudo tee --append /etc/sysctl.d/gt_mhz.conf && sudo echo -e "\ndev.char.226:0.gt_max_freq_mhz = 2200\n"|sudo tee --append /etc/sysctl.d/gt_mhz.conf && sudo echo -e "\ndev.char.226:0.gt_RP1_freq_mhz = 2200\n"|sudo tee --append /etc/sysctl.d/gt_mhz.conf
 EOF
 sudo sysctl --system
+EOF
+sudo rm '/etc/sysctl.d/99-sysctl.conf'
+EOF
+sudo rm /etc/sysctl.d/99-sysctl.conf
+EOF
+sudo rm '/etc/sysctl.conf'
+EOF
+sudo rm /etc/sysctl.conf
+EOF
+rm '/etc/sysctl.d/99-sysctl.conf'
+EOF
+rm /etc/sysctl.d/99-sysctl.conf
+EOF
+rm '/etc/sysctl.conf'
+EOF
+rm /etc/sysctl.conf
+EOF
+cat << EOF > sysctl.conf
+#
+# /etc/sysctl.conf - Configuration file for setting system variables
+# See /etc/sysctl.d/ for additional system variables.
+# See sysctl.conf (5) for information.
+#
+
+#kernel.domainname = example.com
+
+# Uncomment the following to stop low-level messages on console
+# kernel.printk = 3 4 1 3
+
+##############################################################3
+# Functions previously found in netbase
+#
+
+# Uncomment the next two lines to enable Spoof protection (reverse-path filter)
+# Turn on Source Address Verification in all interfaces to
+# prevent some spoofing attacks
+# net.ipv4.conf.default.rp_filter=1
+# net.ipv4.conf.all.rp_filter=1
+
+# Uncomment the next line to enable TCP/IP SYN cookies
+# See http://lwn.net/Articles/277146/
+# Note: This may impact IPv6 TCP sessions too
+# net.ipv4.tcp_syncookies=1
+
+# Uncomment the next line to enable packet forwarding for IPv4
+# net.ipv4.ip_forward=1
+
+# Uncomment the next line to enable packet forwarding for IPv6
+#  Enabling this option disables Stateless Address Autoconfiguration
+#  based on Router Advertisements for this host
+# net.ipv6.conf.all.forwarding=1
+
+
+###################################################################
+# Additional settings - these settings can improve the network
+# security of the host and prevent against some network attacks
+# including spoofing attacks and man in the middle attacks through
+# redirection. Some network environments, however, require that these
+# settings are disabled so review and enable them as needed.
+#
+# Do not accept ICMP redirects (prevent MITM attacks)
+# net.ipv4.conf.all.accept_redirects = 0
+# net.ipv6.conf.all.accept_redirects = 0
+# _or_
+# Accept ICMP redirects only for gateways listed in our default
+# gateway list (enabled by default)
+# net.ipv4.conf.all.secure_redirects = 1
+#
+# Do not send ICMP redirects (we are not a router)
+# net.ipv4.conf.all.send_redirects = 0
+#
+# Do not accept IP source route packets (we are not a router)
+# net.ipv4.conf.all.accept_source_route = 0
+# net.ipv6.conf.all.accept_source_route = 0
+#
+# Log Martian Packets
+# net.ipv4.conf.all.log_martians = 1
+#
+
+###################################################################
+# Magic system request Key
+# 0=disable, 1=enable all, >1 bitmask of sysrq functions
+# See https://www.kernel.org/doc/html/latest/admin-guide/sysrq.html
+# for what other values do
+# kernel.sysrq=438
+EOF
+sudo cp ./sysctl.conf /etc/
+EOF
+cp ./sysctl.conf /etc/
+EOF
+sudo rm ./sysctl.conf
+EOF
+rm ./sysctl.conf
+EOF
+sudo ln -s '/etc/sysctl.conf' '/etc/sysctl.d/99-sysctl.conf'
+EOF
+sudo ln -s /etc/sysctl.conf /etc/sysctl.d/99-sysctl.conf
+EOF
+ln -s '/etc/sysctl.conf' '/etc/sysctl.d/99-sysctl.conf'
+EOF
+ln -s /etc/sysctl.conf /etc/sysctl.d/99-sysctl.conf
+EOF
+sudo sysctl --system
+EOF
+sysctl --system
 EOF
 cat << EOF > securetty
 # /etc/securetty: list of terminals on which root is allowed to login.
