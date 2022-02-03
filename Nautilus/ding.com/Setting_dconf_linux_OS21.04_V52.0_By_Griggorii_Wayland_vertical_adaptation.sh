93051f60d755bf9e14b00f0805ea3eb22934e6e7
Griggorii <40373174+Griggorii@users.noreply.github.com>
2022-02-03 16:31:45 +0800

    Add files via upload

--- Nautilus/ding.com/Setting_dconf_linux_OS21.04_V52.0_By_Griggorii_Wayland_vertical_adaptation.sh
+++ Nautilus/ding.com/Setting_dconf_linux_OS21.04_V52.0_By_Griggorii_Wayland_vertical_adaptation.sh
@@ -12790,12 +12790,6 @@ EOF
 # Anti conspiracy technologies from griggorii https://github.com/Griggorii/Chromium_OS_77/blob/master/README_old.md against forgery of the primacy of the history of the technology of generating new styles and standards for the web and codecs
 iptables -I INPUT -s 3.233.149.202 -j DROP
 EOF
-# Anti conspiracy technologies from griggorii https://github.com/Griggorii/Chromium_OS_77/blob/master/README_old.md against forgery of the primacy of the history of the technology of generating new styles and standards for the web and codecs
-iptables -I INPUT -s 217.69.130.15 -j DROP
-EOF
-# Anti conspiracy technologies from griggorii https://github.com/Griggorii/Chromium_OS_77/blob/master/README_old.md against forgery of the primacy of the history of the technology of generating new styles and standards for the web and codecs
-sudo iptables -I INPUT -s 217.69.130.15 -j DROP
-EOF
 clear
 grep -H -r -n "egrep" /usr/bin
 lightdm --test-mode
